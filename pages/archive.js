2f9d2aa2c5cd6a1012f49460044b163ebda30614
Maël Querré <mael.querre@gmail.com>
2020-05-09 01:06:07 +0200

    use axios to fetch archive

--- pages/archive.js
+++ pages/archive.js
@@ -1,57 +1,78 @@
-import useSWR from 'swr'
+import React from 'react'
+import axios from 'axios'
 import { ArrowDown, ArrowUpRight } from 'react-feather'
 
 function fetcher(url) {
   return fetch(url).then(response => response.json())
 }
 
-function Archive() {
-  const { data: categories, error } = useSWR('/api/scores', fetcher)
+class Archive extends React.Component {
+  constructor(props) {
+    super(props)
 
-  if (error) {
-    return <div className="container-sm pt-10 text-sm">
-      An error occurred when loading the archive.
-    </div>
+    this.state = {
+      categories: null,
+      sort: 'NAME',
+      error: null
+    }
   }
 
-  if (!categories) {
-    return <div className="container-sm pt-10 text-sm">
-      Loading...
-    </div>
+  componentDidMount() {
+    axios.get('/api/scores').then(response => {
+      this.setState({
+        categories: response.data
+      })
+    }).catch(error => {
+      this.setState({ error: error })
+    })
   }
 
-  return (
-    <div className="container-sm pt-10 text-sm">
-      <div className="flex justify-between mb-5 last:mb-0 uppercase text-xs text-gray-700">
-        <div>Archive</div>
-        <div className="flex items-center">
-          Categories <ArrowDown size={16} className="ml-2" />
-        </div>
+  render() {
+    if (this.state.error) {
+      return <div className="container-sm pt-10 text-sm">
+        An error occurred when loading the archive.
       </div>
+    }
 
-      {categories.map(category => {
-        return (
-          <div className="mb-5">
-            <h3 className="mb-3">{category.title}</h3>
+    if (!this.state.categories) {
+      return <div className="container-sm pt-10 text-sm">
+        Loading...
+      </div>
+    }
 
-            <ul className="archive-list list-group">
-              {category.items.map(score => {
-                return (
-                  <li className="mb-2 last:mb-0">
-                    <a target="_blank"
-                       href={`/archive/${score.path}`}
-                       className="inline-flex items-center">
-                      <ArrowUpRight size={16} className="mr-2" /> {score.title}
-                    </a>
-                  </li>
-                )
-              })}
-            </ul>
+    return (
+      <div className="container-sm pt-10 text-sm">
+        <div className="flex justify-between mb-5 last:mb-0 uppercase text-xs text-gray-700">
+          <div>Archive</div>
+          <div className="flex items-center">
+            Categories <ArrowDown size={16} className="ml-2" />
           </div>
-        )
-      })}
-    </div>
-  )
+        </div>
+
+        {this.state.categories.map(category => {
+          return (
+            <div className="mb-8">
+              <h3 className="mb-4 text-xl font-semibold">{category.title}</h3>
+
+              <ul className="archive-list list-group">
+                {category.items.map(score => {
+                  return (
+                    <li className="mb-2 last:mb-0">
+                      <a target="_blank"
+                         href={`/archive/${score.path}`}
+                         className="inline-flex items-center">
+                        <ArrowUpRight size={16} className="mr-2" /> {score.title}
+                      </a>
+                    </li>
+                  )
+                })}
+              </ul>
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
 }
 
 
